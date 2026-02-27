75ed5e115b9a9389280a8fd91ef2512e453ed1a2
Dlizard72 <arigoldring77@gmail.com>
2026-02-26 20:36:26 -0500

    fixed getNames function

--- src/nested.ts
+++ src/nested.ts
@@ -59,7 +59,8 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    const x = questions.map((question: Question): string => question.name);
+    return x;
 }
 
 /***
