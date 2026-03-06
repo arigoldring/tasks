0c31185785d9fc8daea22b04407efa086ad970bc
Dlizard72 <arigoldring77@gmail.com>
2026-03-05 21:19:16 -0500

    implement StartAttempt component with quiz functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-// import { StartAttempt } from "./components/StartAttempt";
+import { StartAttempt } from "./components/StartAttempt";
 // import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -13,16 +13,17 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <hr></hr>
+            <hr />
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
             <ChangeType></ChangeType>
+            <hr />
+            <StartAttempt></StartAttempt>
+
             {/* <StartAttempt></StartAttempt>
-            <hr />
             <TwoDice></TwoDice>
-            <hr />
             <hr />
             <CycleHoliday></CycleHoliday> */}
         </div>
