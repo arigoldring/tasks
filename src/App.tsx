71361393f78b39c4c7c6f6c2ab32e50d917a34df
Dlizard72 <arigoldring77@gmail.com>
2026-03-11 20:51:30 -0400

    Fixed Shove Box

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
-//import { ShoveBox } from "./bad-components/ShoveBox";
+import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
@@ -23,9 +23,8 @@ function App(): React.JSX.Element {
             <ChooseTeam></ChooseTeam> 
             <hr></hr>
             <ColoredBox></ColoredBox>
-            {/* <
             <hr></hr>
-            <ShoveBox></ShoveBox>*/}
+            <ShoveBox></ShoveBox>
             <hr></hr>
             <Counter></Counter>
             <hr />
