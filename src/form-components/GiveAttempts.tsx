f36cf628b8793d66079c34761993647bd66e5d98
Dlizard72 <arigoldring77@gmail.com>
2026-03-17 05:19:32 -0400

    fix: correct props usage in Doubler component and update GiveAttempts functionality

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -3,19 +3,36 @@ import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
     const [attempts, setAttempts] = useState<number>(3);
+    const [inputattempts, setinputattempts] = useState<number>(0);
 
     return (
         <div>
             <Form.Group controlId="set-attempts">
-            <Form.Label>Set Attempts</Form.Label>
-            <Form.Control                type="number"
-                value={attempts}
-                placeholder="Enter number of attempts here"
-                ={(e) => {setAttempts(Number(e.target.value));}}
-            />
-            <Button onClick={() => {setAttempts(attempts - 1)}}>use</Button>
-            <Button onClick={() => {setAttempts(attempts + 1)}}>gain</Button>
-            <p>Remaining Attempts: {attempts}</p>
+                <Form.Label>Set Attempts</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={inputattempts}
+                    placeholder="Enter number of attempts here"
+                    onChange={(e) => {
+                        setinputattempts(Number(e.target.value));
+                    }}
+                />
+                <Button
+                    onClick={() => {
+                        setAttempts(attempts - 1);
+                    }}
+                    disabled={attempts <= 0}
+                >
+                    use
+                </Button>
+                <Button
+                    onClick={() => {
+                        setAttempts(inputattempts);
+                    }}
+                >
+                    gain
+                </Button>
+                <p>Remaining Attempts: {attempts}</p>
             </Form.Group>
         </div>
     );
