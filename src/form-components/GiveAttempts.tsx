c707b85e9899642b0b710a332cd7e91dbfa83206
Dlizard72 <arigoldring77@gmail.com>
2026-03-16 20:24:12 -0400

    saving

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
     const [attempts, setAttempts] = useState<number>(3);
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="set-attempts">
+            <Form.Label>Set Attempts</Form.Label>
+            <Form.Control                type="number"
+                value={attempts}
+                placeholder="Enter number of attempts here"
+                ={(e) => {setAttempts(Number(e.target.value));}}
+            />
+            <Button onClick={() => {setAttempts(attempts - 1)}}>use</Button>
+            <Button onClick={() => {setAttempts(attempts + 1)}}>gain</Button>
+            <p>Remaining Attempts: {attempts}</p>
+            </Form.Group>
         </div>
     );
 }
