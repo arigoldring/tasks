f37e0de5356162674df9ee028b650fec2293d7a5
Dlizard72 <arigoldring77@gmail.com>
2026-03-22 21:20:56 -0400

    moved answer into form

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-export function CheckAnswer({expectedAnswer,}: {expectedAnswer: string;}): React.JSX.Element {
+export function CheckAnswer({
+    expectedAnswer,
+}: {
+    expectedAnswer: string;
+}): React.JSX.Element {
     const [answer, setAnswer] = useState<string>("");
 
     return (
         <div>
             <Form.Group controlId="expected_answer">
-            <Form.Label>Question Here</Form.Label>
-            <Form.Control
-                type="text"
-                value={answer}
-                placeholder="Enter your answer here"
-                onChange={(e) => {setAnswer(e.target.value);}}
-            />
+                <Form.Label>Question Here</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={answer}
+                    placeholder="Enter your answer here"
+                    onChange={(e) => {
+                        setAnswer(e.target.value);
+                    }}
+                />
+                {answer === expectedAnswer && <h3>✔️</h3>}
             </Form.Group>
-            {answer === expectedAnswer && <h3>✔️</h3>}
         </div>
     );
 }
